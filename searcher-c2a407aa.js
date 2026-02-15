44817629806fa590f940e56da890c21d75fdc7a6
zqr10159 <zqr10159@users.noreply.github.com>
2026-02-15 11:31:19 +0000

    deploy: e7503188813218b2a8d55e38505798bfd2da7595

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e13d5ae5.js',
+                path_to_root + 'searchindex-68d4371c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
